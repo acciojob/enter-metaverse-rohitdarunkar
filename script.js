d0d320a475336d2418c44bb7dcd0592d7e8038b3
Rohit Darunkar <139346362+rohitdarunkar@users.noreply.github.com>
2026-01-17 14:22:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const statusPara = document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click", () => {
+  statusPara.innerHTML = "<h1>Entered Metaverse</h1>";
+});
